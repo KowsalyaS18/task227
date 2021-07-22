c96bde6c59939acf789a67915d1dd8d7010f6deb
KowsalyaS18 <87513120+KowsalyaS18@users.noreply.github.com>
2021-07-22 17:06:58 +0530

    Create stringmatch.py
    Write a function which will read a file and check for the given string ( case insensitive check). If the given string is found, the function should return True.

---
+++ stringmatch.py
@@ -0,0 +1,8 @@
+str=input("enter your input")
+file=open("c:Users/AMMA/myproject/Include/testfile.txt","r")
+str1=file.read()
+if str in str2:
+	print("matched")
+else:
+	print("not matched")
+file1.close()
